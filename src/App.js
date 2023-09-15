ee81720ed6f217e669b046df3f86fd6ea41bbdd9
Tasneem Appaduray <tasneem.appaduray@multichoice.com>
2023-09-15 14:45:54 +0200

    Playing around to see if this works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
 
 function App() {
+
+  const searchButton = () => {
+    console.log("You pressed me beech")
+  }
+
   return (
     <div>
      <h1>This is our Search Bar App</h1>
+     <input type="search" ></input>
+     <button onClick={searchButton}>Search me beech</button>
+
     </div>
   );
 }
